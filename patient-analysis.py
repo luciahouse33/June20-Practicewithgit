85c3559c74e20a3e192547c3c3293ecf89051cc0
luciahouse33 <luciahouse33@gmail.com>
2017-06-20 11:14:19 -0400

    Getting patients day 1 data

--- patient-analysis.py
+++ patient-analysis.py
@@ -8,8 +8,11 @@ data = np.loadtxt(fname='data/inflammation1.csv', delimiter=',')
 #Finding the dimensions of the data
 dimensions=shape(data)
 
-print(data)
+#print just the first row of data
+print(data[0])
 
 image-1=plt.plot(data)
 
 plt.show(image-1)
+
+#added a change to the code
